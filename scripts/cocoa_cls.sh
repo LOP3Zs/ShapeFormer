9f777d9d4048adfd280dfc0f5db4b4ee21770408
LOP3Zs <144977553+LOP3Zs@users.noreply.github.com>
2025-09-21 09:54:25 +0700

    Delete scripts directory

--- scripts/cocoa_cls.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=2
-export AISTRON_DATASETS=../data/datasets/
-
-python -W ignore train_net.py \
-    --config-file configs/COCOA-cls/shapeformerv2_R50_FPN_cocoa_cls_8ep_bs2.yaml \
-    --num-gpus 1 
--- scripts/kins2020.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1
-export AISTRON_DATASETS=../data/datasets/
-
-python -W ignore train_net.py \
-    --config-file configs/KINS2020/shapeformerv2_R50_FPN_kins2020_6ep_bs1.yaml \
-    --num-gpus 1 
